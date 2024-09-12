c47e788ff161ff437c581a1821bb7cf4f8da4400
Collinson5076 <collinsabeiku07@gmail.com>
2024-09-11 17:50:28 -0700

    updated 2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -3,4 +3,4 @@
 #Loop through ASCII values for lowercase alphabets
 for alp in range(ord('a'), ord('z') + 1):
     #Print each alphabet without any newline
-    print(chr(alp), end='')
+    print('{:c}'.format(alp), end='')
