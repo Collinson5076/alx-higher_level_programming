7da551e8f903d8b3e24102392f8b6539d56f105c
Collins <collinsabeiku07@gmail.com>
2025-01-17 11:34:10 -0800

    What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-#Python script that fetches https://alx-intranet.hbtn.io/status
+"""Python script that fetches https://alx-intranet.hbtn.io/status"""
 
 import urllib.request
 
-# Define the URL
+"""Define the URL"""
 url = "https://alx-intranet.hbtn.io/status"
 
-# Use a with statement to handle the request
+"""Use a with statement to handle the request"""
 with urllib.request.urlopen(url) as response:
-    # Read the response content
+    """Read the response content"""
     data = response.read()
 
-    # Decode the content to a string
+    """Decode the content to a string"""
     decoded_data = data.decode('utf-8')
 
-    # Print the response details
+    """Print the response details"""
     print("Body response:")
     print(f"    - type: {type(data)}")
     print(f"    - content: {data}")
