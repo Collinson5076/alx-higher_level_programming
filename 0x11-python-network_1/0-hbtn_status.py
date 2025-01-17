3756bc8d122206c09dab657cb86cd6fa44a4fdaf
Collins <collinsabeiku07@gmail.com>
2025-01-17 11:29:37 -0800

    What's my status? #0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+#Python script that fetches https://alx-intranet.hbtn.io/status
+
+import urllib.request
+
+# Define the URL
+url = "https://alx-intranet.hbtn.io/status"
+
+# Use a with statement to handle the request
+with urllib.request.urlopen(url) as response:
+    # Read the response content
+    data = response.read()
+
+    # Decode the content to a string
+    decoded_data = data.decode('utf-8')
+
+    # Print the response details
+    print("Body response:")
+    print(f"    - type: {type(data)}")
+    print(f"    - content: {data}")
+    print(f"    - utf8 content: {decoded_data}")
